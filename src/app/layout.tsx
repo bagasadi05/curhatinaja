58b73685e177e10affd76c36c63eae482c78e1ca
SKI_ Bagas Riyadi <bagaslastname@gmail.com>
2025-07-07 04:14:53 +0000

    saya ingin aplikasiku ini bisa di deploy ke android, jadi berikan saya r

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'CurhatinAja',
   description: 'Ruang amanmu untuk berbagi dan berefleksi.',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -18,6 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&family=Belleza&display=swap" rel="stylesheet" />
+        <meta name="theme-color" content="#e9e7f2" />
       </head>
       <body className="font-body antialiased">
         {children}
